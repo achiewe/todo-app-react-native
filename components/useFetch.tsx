1e5a9c4e56237babde06dfa33069f24fd987a8ef
achiewe <mamukashviliachi706@gmail.com>
2023-12-06 15:09:06 +0400

    add types for data

--- components/useFetch.tsx
+++ components/useFetch.tsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 export default function useFetch() {
+  const [saveInfo, setSaveInfo] = useState<[]>([]);
+
+  console.log(saveInfo);
+
+  const getTask = async () => {
+    const response = await axios.get('http://192.168.0.101:3001/tasks');
+    const data = response.data;
+    setSaveInfo(data);
+  };
   useEffect(() => {
-    const getTask = async () => {
-      const response = await axios.get('http://192.168.0.101:3001/tasks');
-    };
+    getTask();
   }, []);
 }
