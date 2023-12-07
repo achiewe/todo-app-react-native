f5a5caf9f6e45f7ab8eced4068daae6b4378b441
achiewe <mamukashviliachi706@gmail.com>
2023-12-07 14:07:30 +0400

    remove console.log from deleteItem function

--- components/RenderItem.tsx
+++ components/RenderItem.tsx
@@ -27,7 +27,6 @@ export default function renderItem(): JSX.Element {
 
   const deleteItem = async (id: string) => {
     try {
-      console.log('genaa');
       await axios.delete(`http://192.168.0.101:3001/tasks/${id}`);
       getTask();
     } catch (error) {
